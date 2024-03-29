950371948a01e4dc396ed66f72c3d08c8ac73960
amirabbasi <abbasi.amir.89@gmail.com>
2024-03-29 15:08:28 +0330

    1st commit

---
+++ joshankabir/Pages/دعای_جوشن_کبیر.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace joshankabir.Pages
+{
+    public class دعای_جوشن_کبیرModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
